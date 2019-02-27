89b98f2acec46e1788cb0099a784b1fa8e5fbddc
fodisi <fodisi@users.noreply.github.com>
2019-02-26 23:26:33 -0500

    Creating base exchange

--- core/exchange.js
+++ core/exchange.js
@@ -1,3 +1,11 @@
-class Exchange {}
+#!/usr/bin/env node
+
+class Exchange {
+  constructor(options = {}) {
+    this.isTrading = false;
+    this.isExecuting = false;
+    this.options = options;
+  }
+}
 
 export default Exchange;
