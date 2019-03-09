b1193672ad6a54b719fabff778454f3ef5cd7c30
fodisi <fodisi@users.noreply.github.com>
2019-03-09 09:47:46 -0500

    Added credentials sample file

---
+++ credentials-sample.js
@@ -0,0 +1,16 @@
+const credentials = {
+  gdax: {
+    production: {
+      key: 'your-key',
+      secret: 'your-secret',
+      passphrase: 'your-passphrase',
+    },
+    sandbox: {
+      key: 'your-key',
+      secret: 'your-secret',
+      passphrase: 'your-passphrase',
+    },
+  },
+};
+
+export default credentials;
