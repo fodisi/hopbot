6c2b31eb04b51c62062b1e74d39e713886feff0d
fodisi <fodisi@users.noreply.github.com>
2019-03-09 12:55:28 -0500

    renamed debugMode to logLevel; Added log functions based on logLevel

--- src/core/logger.js
+++ src/core/logger.js
@@ -1,9 +1,47 @@
-export function logError(error) {
+/* eslint-disable no-underscore-dangle */
+const LogLevel = {
+  NONE: 0,
+  REGULAR: 1,
+  DEEP: 2,
+};
+
+let _logLevel = LogLevel.NONE;
+
+function setLogLevel(level) {
+  if (level !== LogLevel.NONE && level !== LogLevel.REGULAR && level !== LogLevel.DEEP) {
+    throw new Error(`"${level}" is not a valid "LogLevel`);
+  }
+
+  _logLevel = level;
+}
+
+function logError(error) {
+  console.error(`Error: ${new Date().toUTCString()}`);
   console.error(error);
 }
 
-export function logInfo(info) {
+function logErrorIf(error, minimumLevel = LogLevel.REGULAR) {
+  if (minimumLevel === LogLevel.NONE) {
+    throw new Error('matchLevel cannot be "LogLevelType.NONE"');
+  }
+
+  if (_logLevel >= minimumLevel) {
+    logError(error);
+  }
+}
+
+function logInfo(info) {
   console.info(info);
 }
 
-export default { logError, logInfo };
+function logInfoIf(info, minimumLevel = LogLevel.REGULAR) {
+  if (minimumLevel === LogLevel.NONE) {
+    throw new Error('matchLevel cannot be "LogLevelType.NONE"');
+  }
+
+  if (_logLevel >= minimumLevel) {
+    logInfo(info);
+  }
+}
+
+export { LogLevel, setLogLevel, logError, logErrorIf, logInfo, logInfoIf };
