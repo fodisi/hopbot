87b82d84387e8deb4f3b67cc06665c506038d8d0
Francis Odisi <francis.odisi@shiftmarkets.com>
2019-04-05 16:56:13 -0400

    Add ability to read user input

--- src/hopbot.js
+++ src/hopbot.js
@@ -3,12 +3,34 @@
 // import GdaxExchange from ;
 import GdaxExchange from './plugins/exchanges/GdaxExchange';
 
+const readline = require('readline');
+
+function handleStdinInput(exchange) {
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', (key, data) => {
+    if (data.ctrl && data.name === 'c') {
+      process.exit();
+    } else {
+      switch (key.toUpperCase()) {
+        case 'S':
+          exchange.sell({});
+          break;
+        default:
+          break;
+      }
+    }
+  });
+}
+
 async function start() {
   const gdax = new GdaxExchange();
-  const r = await gdax.connect();
+  const r = gdax.connect();
   console.log(r);
   console.log(gdax.connectionStatus);
   console.log(gdax.getOrderBook().books['BTC-USD'].state());
+  handleStdinInput(gdax);
 }
 
 start();
