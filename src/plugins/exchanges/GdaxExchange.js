3d9fc0beff971e7821ab83c3a6930183095d5f0d
fodisi <fodisi@users.noreply.github.com>
2019-04-29 23:03:49 -0400

    WIP-add _parseOrderParams; implement _sell method

--- src/plugins/exchanges/GdaxExchange.js
+++ src/plugins/exchanges/GdaxExchange.js
@@ -5,6 +5,7 @@ import { AuthenticatedClient, OrderbookSync } from 'gdax';
 import Exchange from '../../core/Exchange';
 import { APIType, APIMode, ConnectionStatus, TradingMode } from '../../core/ExchangeConfig';
 import { LogLevel, logInfoIf, logErrorIf } from '../../core/logger';
+import { OrderType } from '../../core/StrategyConfig';
 
 class GdaxExchange extends Exchange {
   constructor(options = {}) {
@@ -59,12 +60,38 @@ class GdaxExchange extends Exchange {
     return true;
   }
 
+  /**
+   * @description Map default Order param structure to GDAX's order params payload.
+   * @param {Dictionary} params Order params.
+   */
+  _parseOrderParams(params) {
+    const orderParams = {
+      type: params.orderType === OrderType.MARKET ? 'market' : 'limit',
+      side: params.side,
+      product_id: params.productId,
+    };
+
+    // Defaults to size; then, checks for funds.
+    if (params.size && params.size > 0) {
+      orderParams.size = params.size.toString();
+    } else if (params.funds && params.funds > 0) {
+      orderParams.funds = params.funds.toString();
+    }
+
+    if (params.orderType !== OrderType.MARKET && params.price && params.price > 0) {
+      orderParams.price = params.price.toString();
+    }
+
+    return orderParams;
+  }
+
   // eslint-disable-next-line no-unused-vars
   async _sell(params) {
     try {
       let result;
       if (this.tradingMode === TradingMode.LIVE) {
-        result = await this.authClient.sell(params);
+        const orderParams = this._parseOrderParams(params);
+        result = await this.authClient.sell(orderParams);
       } else {
         // TODO: Handle PAPER and SIMULATION scenarios.
         result = Promise.resolve(true);
@@ -121,7 +148,7 @@ class GdaxExchange extends Exchange {
         case 'match':
           this._last_price = data.price;
           this.strategies.executeStrategies({
-            product: data.product_id,
+            productId: data.product_id,
             eventType: data.type,
             size: data.size,
             price: data.price,
