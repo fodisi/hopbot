4be376df525edee17de7777ab5f0033ed19d0bea
fodisi <fodisi@users.noreply.github.com>
2019-04-29 23:04:44 -0400

    WIP-Implementing StopLossTakeProfit strategy

--- src/plugins/strategies/StopLossTakeProfit.js
+++ src/plugins/strategies/StopLossTakeProfit.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-underscore-dangle */
 import Strategy from '../../core/Strategy';
-import { logErrorIf } from '../../core/logger';
+import { logErrorIf, logInfoIf } from '../../core/logger';
+import { OrderType } from '../../core/StrategyConfig';
 
 class StopLossTakeProfit extends Strategy {
   /**
@@ -10,13 +12,13 @@ class StopLossTakeProfit extends Strategy {
    *        'product-id': {
    *            stopLossAt: 0.0,
    *            lossOrderType: '', // StrategyConfig.OrderType
-   *            lossSize: 0., // Amount in Quote Currency (Ex: BTC/USD, size in BTC)
-   *            lossAmount: 0.0, // FUTURE USE - Amount in Quote Currency (Ex: BTC/USD, size in BTC)
+   *            lossSize: 0., // Amount in Base Currency (Ex: BTC/USD, size in BTC)
+   *            lossFunds: 0.0, // FUTURE USE - Amount of Funds in Quote Currency (Ex: BTC/USD, funds in USD)
    *            lossPrice: '', // Used for LIMIT orders
    *            takeProfitAt: 0.0,
    *            profitOrderType: '', // StrategyConfig.OrderType
-   *            profitSize: 0., // Amount in Quote Currency (Ex: BTC/USD, size in BTC)
-   *            profitAmount: 0.0, // FUTURE USE - Amount in Quote Currency (Ex: BTC/USD, size in BTC)
+   *            profitSize: 0., // Amount in Base Currency (Ex: BTC/USD, size in BTC)
+   *            profitFunds: 0.0, // FUTURE USE - Amount of Funds in Quote Currency (Ex: BTC/USD, funds in USD)
    *            profitPrice: '', // Used for LIMIT orders
    *        }
    *    }
@@ -24,27 +26,52 @@ class StopLossTakeProfit extends Strategy {
    */
   constructor(name, config = {}) {
     super(name || 'StopLossProfitSell', config);
+    // Controls if price action is within strategy's price range to avoid false triggers and conflicts with other strategies.
+    this._isWithinPriceRange = false;
+    // TODO: Validate config params.
   }
 
   _execute(data = {}) {
-    if (!data.product || !this.config[data.product]) {
-      logErrorIf(`Product is empty or config not found. Received product: "${data.product}":`);
+    const config = this.config[data.productId];
+
+    if (!config) {
+      logErrorIf(`Strategy ${this._id} - Unable to find configs for product id "${data.productId}".`);
       return;
     }
 
     if (!data.price || data.price <= 0) {
-      logErrorIf(`Invalid param "price". Received: "${data.price}"`);
+      logErrorIf(`Strategy ${this._id} - Param "price" must be greater than zero. Received: "${data.price}"`);
       return;
     }
 
-    const config = this.config[data.product];
-    if (data.price <= config.stopLossAt) {
-      // sell
+    if (!this._isWithinPriceRange && data.price >= config.stopLossAt && data.price <= config.takeProfitAt) {
+      this._isWithinPriceRange = true;
+      logInfoIf(`Strategy ${this._id} - Triggered "isWithinPriceRange @ ${data.price}.`);
       return;
     }
 
-    if (data.price >= config.stopProfitAt) {
+    if (this._isWithinPriceRange && data.price <= config.stopLossAt) {
       // sell
+      const params = {
+        productId: data.productId,
+        orderType: config.lossOrderType,
+        size: config.lossSize,
+        funds: config.lossSize && config.lossSize > 0 ? 0 : config.lossFunds,
+        price: config.lossOrderType === OrderType.MARKET ? 0 : config.lossPrice,
+      };
+      this._exchange.sell(params);
+      return;
+    }
+
+    if (this._isWithinPriceRange && data.price >= config.takeProfitAt) {
+      const params = {
+        productId: data.productId,
+        orderType: config.profitOrderType,
+        size: config.profitSize,
+        funds: config.profitSize && config.profitSize > 0 ? 0 : config.profitFunds,
+        price: config.profitOrderType === OrderType.MARKET ? 0 : config.profitPrice,
+      };
+      this._exchange.sell(params);
     }
   }
 }
